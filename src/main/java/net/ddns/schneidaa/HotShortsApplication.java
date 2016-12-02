a2396fa3dd9aad8aa3e2c8a82b3f2d4d744a120d
PHOENIX\Andreas Schneider <an.schneider@aon.at>
2016-12-02 14:26:22 +0100

    Initial commit

---
+++ src/main/java/net/ddns/schneidaa/HotShortsApplication.java
@@ -0,0 +1,12 @@
+package net.ddns.schneidaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotShortsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotShortsApplication.class, args);
+	}
+}
